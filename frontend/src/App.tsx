c23fd491e7dae00c42d69fd9c7573b9115d72ad2
Aniket-ISTG <aniketsen13012006@gmail.com>
2026-07-12 12:06:36 +0530

    setted up tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    hi there 
-    </>
+    <div className="flex items-center justify-center h-screen bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind works!
+      </h1>
+    </div>
   )
 }
 
-export default App
+export default App;
